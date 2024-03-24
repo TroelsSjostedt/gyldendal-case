aa581c9795871ea52b184437fca141132fe0535a
Troels Sjöstedt Pedersen <sjostedt@gmail.com>
2024-03-24 12:30:15 +0100

    Call backend APIs

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,58 @@
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import { ApiHandler, Problem } from './apiHandler'
+import parse from 'html-react-parser'
+
+const apiHandler = new ApiHandler()
 
 function App() {
+  const [problem, setProblem] = useState<Problem>()
+  const [ctaText, setCtaText] = useState('Tjek mit svar')
+  const [guess, setGuess] = useState<string>()
+
+  useEffect(() => {
+    console.log('Initial useEffect')
+    apiHandler.getProblem().then((p) => setProblem(p))
+  }, [])
+
+  const checkAnswer = () => {
+    console.log('Checking guess: ' + (guess ?? '[undefined]'))
+    if (guess) {
+      apiHandler.checkGuess(guess).then((answer) => {
+        if (!answer.isCorrect) {
+          if (ctaText === 'Tjek mit svar') setCtaText('Prøv igen')
+          else setCtaText('Næste spørgsmål')
+        }
+      })
+    }
+  }
+
   return (
-    <h1 className="text-3xl font-bold text-green-600 underline">
-      Simple React Typescript Tailwind Sample
-    </h1>
-  );
+    <div>
+      {problem ? (
+        <div className="flex flex-col">
+          <h1 className="text-l font-bold">{problem?.introText}</h1>
+          {problem && (
+            <div className="italic">{parse(problem.description)}</div>
+          )}
+          <div className="flex">
+            <div>{problem?.problemText.split('{{input0}}')[0]}</div>
+            <input
+              type="text"
+              className="mx-2 rounded border shadow-inner"
+              onChange={(e) => setGuess(e.target.value)}
+            ></input>
+            <div>{problem?.problemText.split('{{input0}}')[1]}</div>
+          </div>
+          <button className="border" onClick={() => checkAnswer()}>
+            {ctaText}
+          </button>
+        </div>
+      ) : (
+        <div>Indlæser...</div>
+      )}
+    </div>
+  )
 }
 
-export default App;
+export default App
