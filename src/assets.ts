134b7bc6a4e2dd615e93e5480b3434f6984bc811
Troels Sjöstedt Pedersen <sjostedt@gmail.com>
2024-04-08 14:09:20 +0200

    Add Jest and Cypress tests

--- src/assets.ts
+++ src/assets.ts
@@ -4,7 +4,6 @@ import { ReactComponent as WrongAnswer } from './assets/Validations/Wrong answer
 import { ReactComponent as ArrowRetry } from './assets/Arrows/Retry.svg'
 import { ReactComponent as ArrowNext } from './assets/Arrows/Next task.svg'
 import { ReactComponent as DogCorrect } from './assets/Dog/Correct.svg'
-import { ReactComponent as DogCorrecting } from './assets/Dog/Correcting Answer.svg'
 import { ReactComponent as DogDefault } from './assets/Dog/Default.svg'
 import { ReactComponent as DogFirstWrong } from './assets/Dog/First Wrong.svg'
 import { ReactComponent as DogSecondWrong } from './assets/Dog/Second Wrong.svg'
@@ -16,7 +15,6 @@ export {
   ArrowNext,
   pencil,
   DogCorrect,
-  DogCorrecting,
   DogDefault,
   DogFirstWrong,
   DogSecondWrong,
